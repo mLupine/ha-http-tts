2304cb380909518275f2e52bf75c2232dc18a822
Maciej Wilczynski <maciej@lupine.cc>
2019-07-20 23:45:39 +0200

    Initial cleanup

--- custom_components/http_tts/media_player.py
+++ custom_components/http_tts/media_player.py
@@ -1,22 +1,16 @@
 """
-Support for TTS on a Bluetooth Speaker
+Support for TTS on a HTTP endpoint
 
 """
-import voluptuous as vol
 
 from homeassistant.components.media_player import (
     SUPPORT_PLAY_MEDIA,
-    SUPPORT_VOLUME_SET,
     PLATFORM_SCHEMA,
     MediaPlayerDevice)
 from homeassistant.const import (
     CONF_NAME, STATE_OFF, STATE_PLAYING)
 import homeassistant.helpers.config_validation as cv
 
-try:
-   from ..device_tracker import bluetooth_tracker
-except ImportError:
-   pass
 import subprocess
 
 import logging
@@ -27,41 +21,23 @@ import sys
 import time
 
 DEFAULT_NAME = 'TTS Bluetooth Speaker'
-DEFAULT_VOLUME = 0.5
 DEFAULT_CACHE_DIR = "tts"
-DEFAULT_SILENCE_DURATION = 0.0
 
-SUPPORT_BLU_SPEAKER = SUPPORT_PLAY_MEDIA | SUPPORT_VOLUME_SET
+SUPPORT_HTTP_TTS_SPEAKER = SUPPORT_PLAY_MEDIA
 
-CONF_ADDRESS = 'address'
-CONF_VOLUME = 'volume'
+CONF_ENDPOINT = 'endpoint'
 CONF_CACHE_DIR = 'cache_dir'
-CONF_PRE_SILENCE_DURATION = 'pre_silence_duration'
-CONF_POST_SILENCE_DURATION = 'post_silence_duration'
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_VOLUME, default=DEFAULT_VOLUME):
-        vol.All(vol.Coerce(float), vol.Range(min=0, max=1)),
-    vol.Optional(CONF_PRE_SILENCE_DURATION, default=DEFAULT_SILENCE_DURATION):
-        vol.All(vol.Coerce(float), vol.Range(min=0, max=60)),
-    vol.Optional(CONF_POST_SILENCE_DURATION, default=DEFAULT_SILENCE_DURATION):
-        vol.All(vol.Coerce(float), vol.Range(min=0, max=60)),
-    vol.Optional(CONF_CACHE_DIR, default=DEFAULT_CACHE_DIR): cv.string,
-})
 
 _LOGGER = logging.getLogger(__name__)
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Setup the Bluetooth Speaker platform."""
+    """Setup the HTTP TTS Speaker platform."""
     name = config.get(CONF_NAME)
-    address = config.get(CONF_ADDRESS)
-    volume = float(config.get(CONF_VOLUME))
-    pre_silence_duration = float(config.get(CONF_PRE_SILENCE_DURATION))
-    post_silence_duration = float(config.get(CONF_POST_SILENCE_DURATION))
+    endpoint = config.get(CONF_ENDPOINT)
     cache_dir = get_tts_cache_dir(hass, config.get(CONF_CACHE_DIR))
 
-    add_devices([BluetoothSpeakerDevice(hass, name, address, volume, pre_silence_duration, post_silence_duration, cache_dir)])
+    add_devices([BluetoothSpeakerDevice(hass, name, endpoint, cache_dir)])
     return True
 
 def get_tts_cache_dir(hass, cache_dir):
@@ -71,21 +47,17 @@ def get_tts_cache_dir(hass, cache_dir):
     return cache_dir
 
 class BluetoothSpeakerDevice(MediaPlayerDevice):
-    """Representation of a Bluetooth Speaker on the network."""
+    """Representation of an HTTP TTS Speaker on the network."""
 
-    def __init__(self, hass, name, address, volume, pre_silence_duration, post_silence_duration, cache_dir):
+    def __init__(self, hass, name, endpoint, cache_dir):
         """Initialize the device."""
         self._hass = hass
         self._name = name
         self._is_standby = True
         self._current = None
-        self._address = address
-        self._volume = volume
-        self._pre_silence_duration = pre_silence_duration
-        self._post_silence_duration = post_silence_duration
+        self._endpoint = endpoint
         self._cache_dir = self.get_tts_cache_dir(cache_dir)
-        self._tracker = 'custom_components.device_tracker.bluetooth_tracker' in sys.modules
-        _LOGGER.debug('Bluetooth tracker integration:  {}'.format(str(self._tracker)))
+#        _LOGGER.debug('Bluetooth tracker integration:  {}'.format(str(self._tracker)))
 
     def get_tts_cache_dir(self, cache_dir):
         """Get cache folder."""
@@ -117,17 +89,12 @@ class BluetoothSpeakerDevice(MediaPlayerDevice):
     @property
     def supported_features(self):
         """Flag media player features that are supported."""
-        return SUPPORT_BLU_SPEAKER
+        return SUPPORT_HTTP_TTS_SPEAKER
 
     @property
     def volume_level(self):
         """Volume level of the media player (0..1)."""
-        return self._volume
-
-    def set_volume_level(self, volume):
-        """Set volume level, range 0..1."""
-        # self._vlc.audio_set_volume(int(volume * 100))
-        self._volume = volume
+        return 1
 
     def play_media(self, media_type, media_id, **kwargs):
         """Send play commmand."""
